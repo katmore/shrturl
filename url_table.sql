d93954fdd1b37e2e4b6bae7b3413cd5c8785a7e2
Doug Bird <doug@katmore.com>
2012-11-15 13:45:27 -0800

    Update url_table.sql

--- url_table.sql
+++ url_table.sql
@@ -3,11 +3,10 @@
 --
 
 CREATE TABLE IF NOT EXISTS `url` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `code` char(32) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
   `md5` char(32) COLLATE utf8_bin DEFAULT NULL,
   `target` text COLLATE utf8_bin DEFAULT NULL,
   UNIQUE KEY `code` (`code`),
-  UNIQUE KEY `md5` (`md5`),
-  KEY `id` (`id`)
+  UNIQUE KEY `md5` (`md5`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
