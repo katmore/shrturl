077f6db593cc3f6ac9ab209a7444976edd6c7a60
Doug Bird <retran@gmail.com>
2015-10-29 12:31:41 -0700

    Update url_table.sql

--- url_table.sql
+++ url_table.sql
@@ -2,11 +2,14 @@
 -- Table structure for table `url`
 --
 
-CREATE TABLE IF NOT EXISTS `url` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `url` (
+  `id` bigint(20) NOT NULL,
   `code` char(32) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
   `md5` char(32) COLLATE utf8_bin DEFAULT NULL,
-  `target` text COLLATE utf8_bin DEFAULT NULL,
+  `target` text COLLATE utf8_bin,
+  `updated` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   UNIQUE KEY `code` (`code`),
-  UNIQUE KEY `md5` (`md5`)
+  UNIQUE KEY `md5` (`md5`),
+  KEY `id` (`id`),
+  KEY `code_2` (`code`,`target`(255))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
